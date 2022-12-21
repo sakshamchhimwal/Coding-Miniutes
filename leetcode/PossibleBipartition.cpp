5956a556a23d89729222cf9c1191f3b4beb567a8
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2022-12-21 11:03:27 +0530

    ss

---
+++ leetcode/PossibleBipartition.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int,vector<int>> likemap;
+        vector<int> occupiedSet;
+        for(int i=0;i<n;i++){
+            occupiedSet.push_back(0);
+        }
+    }
+};
